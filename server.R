5ab38e28ed3af95e81a82551fd645127b66b13b6
Wenzhao Xu <xuwz.thu@gmail.com>
2016-02-21 18:47:53 -0600

    add correlation and variogram

--- server.R
+++ server.R
@@ -5,7 +5,10 @@ library(zoo)
 library(reshape2)
 library(leaflet)
 library(RColorBrewer)
-
+library(plotly)
+library(sp)
+library(gstat)
+library(raster)
 # conn <- dbConnect(MySQL(), dbname = "DO2014", username="root", password="XuWenzhaO", host="127.0.0.1", port=3306)
 # geoLocation <- dbReadTable(conn,"loggerInfo")
 
@@ -35,6 +38,42 @@ shinyServer(function(input,output,session)
 	})
 	
 
+	visData <- reactive({
+		tmp <- input$selectedID
+		tmp <- paste("logger =",tmp)
+		tmp <- paste(tmp,collapse=" OR ")
+		var <- input$var
+		if(input$dataType=="STD" & input$GroupRange=="daily"){
+			sql <- sprintf("Select date(Time) as Time, STD(%s) as %s, logger from loggerData where %s Group by date(Time),logger",var,var,tmp)
+			# print(sql)
+			timeFormat="%Y-%m-%d"
+		}
+		else if(input$dataType=="STD" & input$GroupRange=="hourly"){
+			sql <- sprintf("Select DATE_FORMAT(Time,'%%Y-%%m-%%d %%H') as Time, STD(%s) as %s, logger from loggerData where %s Group by DATE_FORMAT(Time,'%%Y-%%m-%%d %%H'),logger",var,var,tmp)
+			# print(sql)
+			timeFormat="%Y-%m-%d %H"
+		}
+		else if(input$dataType=="AVG" & input$GroupRange=="daily"){
+			sql <- sprintf("Select date(Time) as Time, AVG(%s) as %s, logger from loggerData where %s Group by date(Time),logger",var,var,tmp)
+			# print(sql)
+			timeFormat="%Y-%m-%d"
+		}
+		else if(input$dataType=="AVG" & input$GroupRange=="hourly"){
+			sql <- sprintf("Select DATE_FORMAT(Time,'%%Y-%%m-%%d %%H') as Time, AVG(%s) as %s, logger from loggerData where %s Group by DATE_FORMAT(Time,'%%Y-%%m-%%d %%H'),logger",var,var,tmp)
+			# print(sql)
+			timeFormat="%Y-%m-%d %H"
+		}
+		else{
+			sql <- paste("Select Time,",var,", logger from loggerData where ",tmp)
+			timeFormat="%Y-%m-%d %H:%M:%S"
+		}
+		data <- sqlQuery(sql,input$year) %>% dcast(Time~logger,value.var=var)
+		# print(data)
+		data <- zoo(subset(data,select=-Time),order.by=strptime(data$Time,format=timeFormat,tz="GMT"))
+
+		return(data)
+	})
+
 	geoData <- reactive({
 		year <- input$year
 		sql <- sprintf("select longitude,latitude,loggerID,bathymetry from loggerInfo where available=1 and loggerPosition='B'")
@@ -49,22 +88,20 @@ shinyServer(function(input,output,session)
   	})
 
 	observe({
-	    pal <- colorpal()
-	    #query the data
-
-	    leafletProxy("mymap", data = geoData()) %>%
-	      clearShapes() %>%
-	      addCircles(layerId=~loggerID,lng=~longitude,lat=~latitude,radius = 3000, weight = 1, color = "#777777",
-	        fillColor = ~pal(bathymetry), fillOpacity = 0.8)
+	   if(input$mapData=="Bathy"){
+		   	pal <- colorpal()
+		    leafletProxy("mymap", data = geoData()) %>%
+		      clearShapes() %>%
+		      addCircles(layerId=~loggerID,lng=~longitude,lat=~latitude,radius = 3000, weight = 1, color = "#777777",
+		        fillColor = ~pal(bathymetry), fillOpacity = 0.8)
+	   } 
   	})
 
 	observe({
 		click<-input$mymap_shape_click
    	 	if(is.null(click))
           return()
-		# print(click)
-		# print(input$mymap_shape_click)
-		# leafletProxy("mymap") %>%clearPopups()
+
 		leafletProxy("mymap")%>%addPopups(click$lng,click$lat, paste(click$id),
 			options=popupOptions(maxHeight=20,zoomAnimation=FALSE))
 
@@ -93,56 +130,84 @@ shinyServer(function(input,output,session)
 
 		leafletProxy("mymap")%>%clearPopups()%>%addPopups(data=lastData,lng=~longitude,lat=~latitude,paste(lastData$loggerID),
 			options=popupOptions(maxHeight=20,zoomAnimation=FALSE))
-		tmp <- paste("logger =",tmp)
-		tmp <- paste(tmp,collapse=" OR ")
 		
-		# print(sql)
-		if(input$dataType=="STD" & input$GroupRange=="daily"){
-			sql <- sprintf("Select date(Time) as Time, STD(%s) as %s, logger from loggerData where %s Group by date(Time),logger",var,var,tmp)
-			# print(sql)
-			timeFormat="%Y-%m-%d"
-		}
-		else if(input$dataType=="STD" & input$GroupRange=="hourly"){
-			sql <- sprintf("Select DATE_FORMAT(Time,'%%Y-%%m-%%d %%H') as Time, STD(%s) as %s, logger from loggerData where %s Group by DATE_FORMAT(Time,'%%Y-%%m-%%d %%H'),logger",var,var,tmp)
-			# print(sql)
-			timeFormat="%Y-%m-%d %H"
-		}
-		else if(input$dataType=="AVG" & input$GroupRange=="daily"){
-			sql <- sprintf("Select date(Time) as Time, AVG(%s) as %s, logger from loggerData where %s Group by date(Time),logger",var,var,tmp)
-			# print(sql)
-			timeFormat="%Y-%m-%d"
-		}
-		else if(input$dataType=="AVG" & input$GroupRange=="hourly"){
-			sql <- sprintf("Select DATE_FORMAT(Time,'%%Y-%%m-%%d %%H') as Time, AVG(%s) as %s, logger from loggerData where %s Group by DATE_FORMAT(Time,'%%Y-%%m-%%d %%H'),logger",var,var,tmp)
-			# print(sql)
-			timeFormat="%Y-%m-%d %H"
-		}
-		else{
-			sql <- paste("Select Time,",var,", logger from loggerData where ",tmp)
-			timeFormat="%Y-%m-%d %H:%M:%S"
-		}
-		data <- sqlQuery(sql,input$year) %>% dcast(Time~logger,value.var=var)
-		# print(data)
-		data <- zoo(subset(data,select=-Time),order.by=strptime(data$Time,format=timeFormat))
-		# print(head(data))
+		data <- visData()
 		if(input$scale){
 			data <- scale(data)
 		}
-		
-		# timeMiddle <- as.POSIXct(input$myDate)
-		# timeStart <- timeMiddle - 12*3600
-		# timeEnd <- timeMiddle + 12*3600
 
-		# dygraph(data, main = "Time Series") %>% dyRangeSelector(dateWindow = c(timeStart, timeEnd)) 
-		# print(head(data))
-		# if(length(tmp$input)==2){
-			# output$cor <- renderText(paste("Correlation:",cor(data[,2],data[3])))
-		# }
-		# else{
-			# output$cor <- renderText("Choose two loggers")
-		# }
-		dygraph(data, main = "Time Series") %>% dyRangeSelector()
+		if(!input$twoy){
+			dygraph(data, main = "Time Series") %>% dyRangeSelector(retainDateWindow=TRUE)
+		}
+		else{
+			if(length(tmp)==2){
+				name2=names(data)[2]
+				dygraph(data, main = "Time Series") %>% dyRangeSelector(retainDateWindow=TRUE) %>%  dySeries(name2, axis = 'y2')
+			}
+		}
 	})
 
+	output$corr <- renderTable({
+		if(is.null(input$selectedID))
+			return()
+		if(input$dataType=="Raw")
+			return()
+		# if(input$var==""){}
+		return(cor(visData(),use="pairwise.complete.obs"))
+	})
+
+
+	spatialData <- reactive({
+		tmp <- input$selectedID
+		tmp <- paste("logger =",tmp)
+		tmp <- paste(tmp,collapse=" OR ")
+		var <- input$var
+
+		QueryDay <- input$myDate
+		QueryHour <- input$myHour
+
+		# print("hello")
+		if(input$GroupRange=="daily"){
+			sql <- sprintf("Select date(Time) as Time, AVG(%s) as %s, logger from loggerData where (%s) and date(Time) = '%s' Group by date(Time),logger",var,var,tmp,QueryDay)
+		}
+		else{
+			sql <- sprintf("Select date(Time) as Time, AVG(%s) as %s, logger from loggerData where (%s) and date(Time) = '%s' Group by date(Time),logger",var,var,tmp,QueryDay)
+
+		}
+		# print(sql)
+		data <- sqlQuery(sql,input$year)
+		data <- merge(data,geoData(),by.x = "logger", by.y = "loggerID",all.y = FALSE)
+		data$id <- 1:nrow(data)
+		return(data)
+	})
+
+	output$Variogram <- renderPlotly({
+		if(is.null(input$selectedID)){
+			return()
+		}
+		if(length(input$selectedID)<2){
+			return()
+		}
+		spdata <- spatialData()
+		names(spdata)[3]="var"
+		coordinates(spdata)= ~longitude+latitude
+		projection(spdata)=CRS("+init=epsg:4326")
+		print(spdata)
+		eq <- paste("var",input$equation)
+		print(eq)
+		v <- data.frame(variogram(as.formula(eq),data=spdata,cloud=T,cutoff=10000))
+		print(v)
+		v$leftLogger <- spdata$logger[v$left]
+		v$rightLogger <- spdata$logger[v$right]
+
+		v$leftValue <- spdata$var[v$left]
+		v$rightValue <- spdata$var[v$right]
+
+		p <- plot_ly(v, x = dist, y=gamma, mode="markers",hoverinfo = "text",
+          text = paste(v$leftLogger,"(",round(v$leftValue,2),")--",v$rightLogger,"(",round(v$rightValue,2),")",sep=""))
+
+		p
+
+	})
 
 })
